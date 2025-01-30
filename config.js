4a761df825fba075b5514a87167867ee72db4c84
MaximeFlorent <46501859+MaximeFlorent@users.noreply.github.com>
2025-01-30 08:07:15 +0000

    Save Js tools

--- config.js
+++ config.js
@@ -2,3 +2,16 @@
 const listeMots = ["Bonjour", "Salut", "Coucou"]
 const listePhrases = ["Bonjour, comment allez-vous ?", "Salut, ça va ?", "Coucou ça va ?"]
 const listBoutons = document.querySelectorAll("button")
+
+/**
+ * Initialiser un dépôt Git et envoyer les modifications : Si tu veux transformer le répertoire en dépôt Git, tu peux initialiser un nouveau dépôt Git en exécutant :
+git init
+git add .
+git commit -m "Save Js tools"
+Pour envoyer les modifications sur mon GitHub
+First you need to generate a new token.
+My account - Setting - Developer Setting - Personal Access Token - Generate New Token
+After you generate the token,
+git remote set-url origin https://TOKEN@github.com/MaximeFlorent/JavascriptTools.git
+git push --force origin main
+ */
